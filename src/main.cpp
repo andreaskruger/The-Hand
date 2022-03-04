bca874acae7a93db43c860d8a85fef581f781d7c
PaulineNassl <paunas@student.chalmers.se>
2022-03-04 14:15:07 +0100

    readflex

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,33 @@
 #include <Arduino.h>
 #include <readFingers.h>
 
+const int flexpin = 32;
 
 void setup() {
   initFlexRead();
+  Serial.begin(115200);
 }
 
 void loop() {
 
 
   //for reading all flexsensors and creating a list to be send
-  float *pointer = getAngleList();
-  float newAngleList[] = {};
+  //float *pointer = getAngleList();
+  //float newAngleList[] = {};
 
-  for(int i = 0; i < 10; i++){
-    newAngleList[i] = *pointer;
-    pointer++;
-  }
+  //for(int i = 0; i < 10; i++){
+  //  newAngleList[i] = *pointer;
+  //  pointer++;
+  //}
 
   //if we only want to test and sending via serial port this is simpler
-  int pins[] = {32};
-  for(int i = 0; i < sizeof(pins); i++){
-        Serial.println( String( getAngle(readResistance(pins[i])) ) );
-    }
+  //int pins[] = {32};
+  //for(int i = 0; i < sizeof(pins); i++){
+  //      Serial.println( String( getAngle(readResistance(pins[i])) ) );
+  //  }
 
+  Serial.println(String( getAngle(readResistance(flexpin)) ) );
+
+
+  delay(200);
 }
-
--- src/readFingers.h
+++ src/readFingers.h
@@ -1,15 +1,18 @@
 #include <Arduino.h>
+#include <ADS1118.h>
+#include <SPI.h>
+
 
 const int f1PIP_Pin = 32;			// Pin connected to voltage divider output for the 1st fingers PIP joint
-int pins[] = {32};
+int fingerpins[] = {32};
 
 // Change these constants according to your project's design
-const float VCC = 5;			// voltage at Ardunio 5V line
-const float R_DIV = 1000.0;	// resistor used to create a voltage divider
+const float VCC = 3.3;			// voltage at Ardunio 5V line
+const float R_DIV = 100000.0;	// resistor used to create a voltage divider
 
 void initFlexRead(){
-    for(int i = 0; i < sizeof(pins); i++){
-        pinMode(pins[i], INPUT);
+    for(int i = 0; i < sizeof(fingerpins); i++){
+        pinMode(fingerpins[i], INPUT);
     }
 }
 
@@ -28,9 +31,23 @@ float getAngle(float resistance){
 float *getAngleList(){
     float angles[] = {};
 
-    for(int i = 0; i < sizeof(pins); i++){
-        angles[i] = getAngle(readResistance(pins[i]));
+    for(int i = 0; i < sizeof(fingerpins); i++){
+        angles[i] = getAngle(readResistance(fingerpins[i]));
     }
 
     return angles;
+}
+
+void printAngleList(){
+    Serial.println('Angle list: ');
+    Serial.println(sizeof(fingerpins));
+    delay(2000);
+
+    for(int i = 1; i <= 1; i++){     // OBS hårdkodad!!!!!
+        //Serial.println('index: ' + String(i) + ' Value: ' + String( getAngle(readResistance(pins[i])) ) );
+       
+        delay(1000);
+        Serial.println('index: ' + String(i) + ' Value: ' + String(readResistance(fingerpins[i])));
+    }
+
 }
