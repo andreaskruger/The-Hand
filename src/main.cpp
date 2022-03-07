69f33c0959f87698470a799e11cf7c258cd7953c
Andreas Axén <andreas.axen.kruger@gmail.com>
2022-03-07 14:48:35 +0100

    Cleand up talkyBoii

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,23 @@
 
 const int f1PIP_Pin = 32;			// Pin connected to voltage divider output for the 1st fingers PIP joint
 const int f1MCP_Pin = 33;			// Pin connected to voltage divider output for the 1st fingers MCP joint
+const int pinList[] = {5,8,12,13,10,11};
+const int sizeList = sizeof(pinList)/sizeof(int);
+float fingerAngles[8];
 
 void setup() {
   Serial.begin(115200);
-  initFlexSensor(f1PIP_Pin);
-  initFlexSensor(f1MCP_Pin);
+  
   delay(100);
   getMACAdress();
   init_wifi();
+  for(int i = 0; i<sizeList; i++){
+    initFlexSensor(pinList[i]);
+  }
 }
 
 void loop() {
-
+  fingerAngles[0] = getAngle();
   float angle1PIP = getAngle(f1PIP_Pin);
   float angle1MCP = getAngle(f1MCP_Pin);
 
