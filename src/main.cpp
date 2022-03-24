8f1a089d98e172ad82a57ea47de52c99e2e80fa8
Linneasedin <94436291+Linneasedin@users.noreply.github.com>
2022-03-24 13:23:30 +0100

    New_board

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
+#include <ADS1118.h>
 #include "TalkyBoi.h"
 
 // Pin connected to voltage divider output. used for analogRead to get resistance of the flexsensors
+const int s0 = 36;
+const int s1 = 39;
+const int s2 = 32;
+const int s3 = 33;
+const int SIG_pin = 34;
+/*
 const uint8_t thumbIP_Pin = ads1118.AIN_0;
-const uint8_t thumbMCP_Pin = ads1118.AIN_1;
-const uint8_t f1PIP_Pin = ads1118.AIN_2;			
+const uint8_t thumbMCP_Pin =  ads1118.AIN_1;
+const uint8_t f1PIP_Pin =  ads1118.AIN_2;			
 const uint8_t f1MCP_Pin = ads1118.AIN_3;			
 const int f2PIP_Pin = 36;
 const int f2MCP_Pin = 39;
@@ -16,18 +23,70 @@ const int pinList[] = {thumbIP_Pin, thumbMCP_Pin, f1PIP_Pin, f1MCP_Pin, f2PIP_Pi
 const int sizeList = sizeof(pinList)/sizeof(int);
 float fingerAngles[10] = {0,0,0,0,0,0,0,0,0,0};
 int sendID = 0;
+*/
 
 int buttonRun = 0;
 int state = 0;
 
-/**
- * Initiates the ADC breakout board.
-*/
-void initBoard(){
+
+ // Initiates the ADC breakout board.
+/*
+void initBoard2(){
     ads1118.begin();    // Initialize board
     ads1118.setSamplingRate(ads1118.RATE_860SPS);   // highest sampling rate possible
     ads1118.setFullScaleRange(ads1118.FSR_4096);    // 12 bit
 }
+*/
+
+void initBoard(){
+  
+
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+
+  digitalWrite(s0, LOW);
+  digitalWrite(s1, LOW);
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, LOW);
+}
+
+int readMux(int channel){
+  int controlPin[] = {s0, s1, s2, s3};
+
+  int muxChannel[16][4]={
+    {0,0,0,0}, //channel 0
+    {1,0,0,0}, //channel 1
+    {0,1,0,0}, //channel 2
+    {1,1,0,0}, //channel 3
+    {0,0,1,0}, //channel 4
+    {1,0,1,0}, //channel 5
+    {0,1,1,0}, //channel 6
+    {1,1,1,0}, //channel 7
+    {0,0,0,1}, //channel 8
+    {1,0,0,1}, //channel 9
+    {0,1,0,1}, //channel 10
+    {1,1,0,1}, //channel 11
+    {0,0,1,1}, //channel 12
+    {1,0,1,1}, //channel 13
+    {0,1,1,1}, //channel 14
+    {1,1,1,1}  //channel 15
+  };
+
+  //loop through the 4 sig
+  for(int i = 0; i < 4; i ++){
+    digitalWrite(controlPin[i], muxChannel[channel][i]);
+  }
+
+  //read the value at the SIG pin
+  int val = analogRead(SIG_pin);
+
+  //return the value
+  return val;
+}
+
+
 
 /**
  * Toggles the state of the microcontroller.
@@ -51,8 +110,8 @@ void interuptFunc(){
 void setup() {
   Serial.begin(115200);
   delay(100);
-  getMACAdress();                          //MAC adress is run for the WIFI to work, the OTHER wearers MAC adress shall be written in this code and vice versa.
-  init_wifi();                             // Initiate ESP_NOW
+  // getMACAdress();                          //MAC adress is run for the WIFI to work, the OTHER wearers MAC adress shall be written in this code and vice versa.
+  // init_wifi();                             // Initiate ESP_NOW
   initBoard();                             // Initiate breakout board                  
   //attachInterrupt(17, interuptFunc, HIGH); // interupt for start/stop button
 }
@@ -63,7 +122,15 @@ void loop() {
 
   send(sendID, fingerAngles[0], fingerAngles[1], fingerAngles[2], fingerAngles[3], fingerAngles[4], fingerAngles[5], fingerAngles[6], fingerAngles[7], fingerAngles[8], fingerAngles[9]);
   */
-  delay(100);
+  int val1 = readMux(0);
+  int val2 = readMux(1);
+  Serial.print("val 1: ");
+  Serial.println(val1);
+  Serial.print("val 2: ");
+  Serial.println(val2);
+  
+  delay(250);
+
 }
 
 
--- src/sensorClass.cpp
+++ src/sensorClass.cpp
@@ -2,27 +2,31 @@
  * This class file is used to create flex sensor objects that handle all reading
  * and filtering of each sensor signal.
 */
+/*
 #include <Arduino.h>
 #include <ADS1118.h>
 #include <SPI.h>
 #include "SensorClass.h"
 #include "Config.h"
 
-
+*/
 /**
  * Constructor
  * @param pin the pin to which the flex sensor is connected to.
  * @param ads the ADC breakout board object which the flexsensor is connected to.
 */
+/*
 sensor::sensor(int pin, ADS1118 ads){
     m_pin = pin;
     ads1118 = ads;
 }
+*/
 
 /**
  * Performs a measurement of the sensor and adds it to the median filter.
  * @return a float with the median angle value.
 */
+/*
 float sensor::getAngle(){
     float angle = getResistance(m_pin) * 2.6716/1000 - 30.2045 - 60; //From Matlab calibration
 
@@ -30,6 +34,7 @@ float sensor::getAngle(){
 
     return m_f.getMedian();
 }
+*/
 
 /**
  * Helper method for getAngle().
@@ -37,6 +42,7 @@ float sensor::getAngle(){
  * @param pin the pin to take a reading from.
  * @return a float with the resistance of the flex sensor.
 */
+/*
 float sensor::getResistance(int pin){ 
     float Vflex;
 
@@ -45,4 +51,5 @@ float sensor::getResistance(int pin){
     float Rflex = R_DIV * (VCC / Vflex - 1.0);
     return Rflex;
 }
+*/
 
